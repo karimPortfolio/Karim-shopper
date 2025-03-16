1aba5d95867c4c51ec1ed9dbcc47685c0853f234
KarimPortfolio <mohamedkarmballa19@gmail.com>
2025-03-16 13:33:15 +0000

    Added brand column to products table migrations

--- database/migrations/2025_03_16_133102_add_brand_column_to_products_table.php
+++ database/migrations/2025_03_16_133102_add_brand_column_to_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('brand')->nullable()->after('name');
+            $table->string('brand')->nullable();
         });
     }
 
